670c0d8e44de3d27379d2f7f9e253195c04dcc00
bedrock-adam <adammikulas@gmail.com>
2024-01-26 20:28:40 +1100

    commmit changes

--- app/sidekiq/hard_job.rb
+++
@@ -1,9 +0,0 @@
-class HardJob
-  include Sidekiq::Job
-
-  def perform(args)
-    message = Message.find(args['message_id'])
-
-    logger.info "[#{message.id}] processed"
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_164920) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_14_120827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
-    t.bigint "account_id", null: false
-    t.bigint "user_id", null: false
-    t.text "body_class_name", null: false
-    t.jsonb "body_json", null: false
+  create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "outboxer_exceptions", force: :cascade do |t|
-    t.bigint "message_id", null: false
-    t.text "class_name", null: false
-    t.text "message_text", null: false
+    t.bigint "outboxer_message_id", null: false
+    t.string "class_name", null: false
+    t.string "message_text", null: false
     t.text "backtrace", array: true
     t.datetime "created_at", null: false
-    t.index ["message_id"], name: "index_outboxer_exceptions_on_message_id"
+    t.index ["outboxer_message_id"], name: "index_outboxer_exceptions_on_outboxer_message_id"
   end
 
   create_table "outboxer_messages", force: :cascade do |t|
-    t.string "message_type", null: false
-    t.bigint "message_id", null: false
-    t.text "status", null: false
+    t.string "status", null: false
+    t.string "outboxer_messageable_type", null: false
+    t.bigint "outboxer_messageable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["message_type", "message_id"], name: "index_outboxer_messages_on_message"
-    t.index ["message_type", "message_id"], name: "index_outboxer_messages_on_message_type_and_message_id", unique: true
+    t.index ["outboxer_messageable_type", "outboxer_messageable_id"], name: "index_outboxer_messages_on_outboxer_messageable"
     t.index ["status", "created_at"], name: "index_outboxer_messages_on_status_and_created_at"
   end
 
-  add_foreign_key "outboxer_exceptions", "outboxer_messages", column: "message_id"
+  add_foreign_key "outboxer_exceptions", "outboxer_messages"
 end
