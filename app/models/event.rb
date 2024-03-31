58e5f81df5499ddf4c167ffc949e15a347402c1a
Adam Mikulasev <adam.mikulasev@creditorwatch.com.au>
2024-03-31 20:51:14 +1100

    update outboxer interfaces

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
-  include Outboxer::Messageable
+  after_create do |event|
+    Outboxer::Message.backlog!(messageable_type: event.class.name, messageable_id: event.id)
+  end
 end
