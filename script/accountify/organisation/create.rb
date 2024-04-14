f91645fa8de9b86b6ad1687a924bc6474e8cdaa0
Adam Mikulasev <adam.mikulasev@creditorwatch.com.au>
2024-04-14 21:00:08 +1000

    get foreman working end to end

--- script/accountify/organisation/create.rb
+++ script/accountify/organisation/create.rb
@@ -1,6 +1,28 @@
-user = { id: 1 }
-tenant = { id: 1 }
+iam_user = { id: 1 }
+iam_tenant = { id: 1 }
 
-organisation = Accountify::Organisation.create(user: user, tenant: tenant, name: 'Big Bin Corp')
-Accountify::Organisation.update(user: user, tenant: tenant, id: organisation[:id], name: 'Big Bin Corp updated')
-Accountify::Organisation.delete(user: user, tenant: tenant, id: organisation[:id])
+$running = true
+
+Signal.trap("INT") do
+  puts "Shutdown requested, terminating..."
+
+  $running = false
+end
+
+while $running
+  begin
+    organisation, event = Accountify::Organisation
+      .create(
+        iam_user: iam_user,
+        iam_tenant: iam_tenant,
+        name: 'Big Bin Corp')
+
+    puts "Created (organisation,event)=(#{organisation[:id]},#{event[:id]}) for #{iam_user[:id]} at #{Time.now}"
+
+    sleep 5
+  rescue => e
+    puts "An error occurred: #{e.message}"
+  end
+end
+
+puts "Script has been terminated."
