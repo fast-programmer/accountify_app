f8d4691feeb345f0c0f4062f43c64dd42e2bfa2d
bedrock-adam <adammikulas@gmail.com>
2024-04-14 21:15:58 +1000

    delete filter param logging too

--- config/initializers/filter_parameter_logging.rb
+++
@@ -1,5 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
-]
