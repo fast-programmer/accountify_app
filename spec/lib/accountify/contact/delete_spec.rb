739130d067ebcd34f108c0a03aff90ad4debd9f2
bedrock-adam <adammikulas@gmail.com>
2024-06-01 19:24:34 +1000

    update syntax

--- spec/lib/accountify/contact/delete_spec.rb
+++ spec/lib/accountify/contact/delete_spec.rb
@@ -3,39 +3,46 @@ require 'rails_helper'
 module Accountify
   RSpec.describe Contact do
     let(:iam_user_id) { 12 }
+
     let(:iam_tenant_id) { 4 }
 
-    let(:organisation) { create(:accountify_organisation, iam_tenant_id: iam_tenant_id) }
+    let(:organisation) do
+      create(:accountify_organisation, iam_tenant_id: iam_tenant_id)
+    end
+
     let(:first_name) { 'John' }
     let(:last_name) { 'Doe' }
     let(:email) { 'john.doe@example.com' }
 
+    let(:id) do
+      create(:accountify_contact, iam_tenant_id: iam_tenant_id,
+        organisation_id: organisation.id,
+        first_name: first_name,
+        last_name: last_name,
+        email: email
+      ).id
+    end
+
+    let!(:event_id) do
+      Contact.delete(iam_user_id: iam_user_id, iam_tenant_id: iam_tenant_id, id: id)
+    end
+
+    let(:contact) do
+      Models::Contact.where(iam_tenant_id: iam_tenant_id).find_by!(id: id)
+    end
+
+    let(:event) do
+      Contact::DeletedEvent
+        .where(iam_tenant_id: iam_tenant_id)
+        .find_by!(id: event_id)
+    end
+
     describe '.delete' do
-      let(:id) do
-        create(:accountify_contact, iam_tenant_id: iam_tenant_id,
-          organisation_id: organisation.id,
-          first_name: first_name,
-          last_name: last_name,
-          email: email
-        ).id
-      end
-
       it "updates model deleted at" do
-        Contact.delete(iam_user_id: iam_user_id, iam_tenant_id: iam_tenant_id, id: id)
-
-        contact = Models::Contact.where(iam_tenant_id: iam_tenant_id).find_by!(id: id)
-
         expect(contact.deleted_at).not_to be_nil
       end
 
       it 'creates deleted event' do
-        event_id = Contact.delete(
-          iam_user_id: iam_user_id, iam_tenant_id: iam_tenant_id, id: id)
-
-        event = Contact::DeletedEvent
-          .where(iam_tenant_id: iam_tenant_id)
-          .find_by!(id: event_id)
-
         expect(event.body).to include(
           'contact' => a_hash_including(
             'id' => id,
@@ -43,20 +50,10 @@ module Accountify
       end
 
       it 'associates event with model' do
-        event_id = Contact.delete(
-          iam_user_id: iam_user_id, iam_tenant_id: iam_tenant_id, id: id)
-
-        contact = Models::Contact
-          .where(iam_tenant_id: iam_tenant_id)
-          .find_by!(id: id)
-
         expect(contact.events.last.id).to eq event_id
       end
 
       it 'queues event created job' do
-        event_id = Contact.delete(
-          iam_user_id: iam_user_id, iam_tenant_id: iam_tenant_id, id: id)
-
         expect(Event::CreatedJob.jobs).to match([
           hash_including(
             'args' => [
--- spec/lib/accountify/contact/update_spec.rb
+++ spec/lib/accountify/contact/update_spec.rb
@@ -3,52 +3,52 @@ require 'rails_helper'
 module Accountify
   RSpec.describe Contact do
     let(:iam_user_id) { 12 }
+
     let(:iam_tenant_id) { 4 }
 
     let(:organisation) { create(:accountify_organisation, iam_tenant_id: iam_tenant_id) }
+
     let(:first_name) { 'John' }
     let(:last_name) { 'Doe' }
     let(:email) { 'john.doe@example.com' }
 
+    let(:id) do
+      create(:accountify_contact,
+        iam_tenant_id: iam_tenant_id,
+        organisation_id: organisation.id,
+        first_name: first_name,
+        last_name: last_name,
+        email: email).id
+    end
+
+    let!(:event_id) do
+      Contact.update(
+        iam_user_id: iam_user_id,
+        iam_tenant_id: iam_tenant_id,
+        id: id,
+        first_name: 'Johnny',
+        last_name: 'Doherty',
+        email: 'johnny.doherty@coolbincompany.org')
+    end
+
+    let(:contact) do
+      Models::Contact.where(iam_tenant_id: iam_tenant_id).find_by!(id: id)
+    end
+
+    let(:event) do
+      Contact::UpdatedEvent
+        .where(iam_tenant_id: iam_tenant_id)
+        .find_by!(id: event_id)
+    end
+
     describe '.update' do
-      let(:id) do
-        create(:accountify_contact,
-          iam_tenant_id: iam_tenant_id,
-          organisation_id: organisation.id,
-          first_name: first_name,
-          last_name: last_name,
-          email: email).id
-      end
-
       it 'updates model' do
-        Contact.update(
-          iam_user_id: iam_user_id,
-          iam_tenant_id: iam_tenant_id,
-          id: id,
-          first_name: 'Johnny',
-          last_name: 'Doherty',
-          email: 'johnny.doherty@coolbincompany.org')
-
-        contact = Models::Contact.where(iam_tenant_id: iam_tenant_id).find_by!(id: id)
-
         expect(contact.first_name).to eq('Johnny')
         expect(contact.last_name).to eq('Doherty')
         expect(contact.email).to eq('johnny.doherty@coolbincompany.org')
       end
 
       it 'creates updated event' do
-        event_id = Contact.update(
-          iam_user_id: iam_user_id,
-          iam_tenant_id: iam_tenant_id,
-          id: id,
-          first_name: 'Johnny',
-          last_name: 'Doherty',
-          email: 'johnny.doherty@coolbincompany.org')
-
-        event = Contact::UpdatedEvent
-          .where(iam_tenant_id: iam_tenant_id)
-          .find_by!(id: event_id)
-
         expect(event.body).to eq({
           'contact' => {
             'id' => id,
@@ -58,30 +58,10 @@ module Accountify
       end
 
       it 'associates event with model' do
-        event_id = Contact.update(
-          iam_user_id: iam_user_id,
-          iam_tenant_id: iam_tenant_id,
-          id: id,
-          first_name: 'Johnny',
-          last_name: 'Doherty',
-          email: 'johnny.doherty@coolbincompany.org')
-
-        contact = Models::Contact
-          .where(iam_tenant_id: iam_tenant_id)
-          .find_by!(id: id)
-
         expect(contact.events.last.id).to eq(event_id)
       end
 
       it 'queues event created job' do
-        event_id = Contact.update(
-          iam_user_id: iam_user_id,
-          iam_tenant_id: iam_tenant_id,
-          id: id,
-          first_name: 'Johnny',
-          last_name: 'Doherty',
-          email: 'johnny.doherty@coolbincompany.org')
-
         expect(Event::CreatedJob.jobs).to match([
           hash_including(
             'args' => [
