20c3c73e333730c7eb71756393b96610464203d4
bedrock-adam <adammikulas@gmail.com>
2024-06-28 07:48:39 +1000

    add created job specs

--- app/jobs/event/created_job.rb
+++ app/jobs/event/created_job.rb
@@ -6,7 +6,19 @@ module Event
 
     def perform(args)
       case args['type']
+      when 'Accountify::Invoice::IssuedEvent'
+        Accountify::AgedReceivablesReport::GenerateJob.perform_async({
+          'iam_tenant_id' => args['iam_tenant_id'] })
       when 'Accountify::Invoice::UpdatedEvent'
+        Accountify::AgedReceivablesReport::GenerateJob.perform_async({
+          'iam_tenant_id' => args['iam_tenant_id'] })
+      when 'Accountify::Invoice::PaidEvent'
+        Accountify::AgedReceivablesReport::GenerateJob.perform_async({
+          'iam_tenant_id' => args['iam_tenant_id'] })
+      when 'Accountify::Invoice::VoidedEvent'
+        Accountify::AgedReceivablesReport::GenerateJob.perform_async({
+          'iam_tenant_id' => args['iam_tenant_id'] })
+      when 'Accountify::Invoice::DeletedEvent'
         Accountify::AgedReceivablesReport::GenerateJob.perform_async({
           'iam_tenant_id' => args['iam_tenant_id'] })
       end
