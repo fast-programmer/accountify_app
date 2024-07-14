217fb75f14069b378562d3a83eaff063e19b338b
bedrock-adam <adammikulas@gmail.com>
2024-07-14 20:08:44 +1000

    further simplify

--- lib/accountify/models/organisation.rb
+++ lib/accountify/models/organisation.rb
@@ -3,8 +3,7 @@ module Accountify
     class Organisation < ActiveRecord::Base
       self.table_name = 'accountify_organisations'
 
-      has_many :events, -> { order(created_at: :asc) },
-        as: :eventable, class_name: 'Event'
+      has_many :events, -> { order(created_at: :asc) }, as: :eventable
 
       has_one :invoice_status_summary
     end
