5ea4dea6ba8b0437118b4b62ebb0fb3b32b5697f
Adam Mikulasev <adammikulas@gmail.com>
2024-11-16 11:32:10 +1100

    remove Outboxer (#23)
    remove outbxoer

--- lib/event.rb
+++ lib/event.rb
@@ -1,15 +1,12 @@
 class Event < ActiveRecord::Base
   self.table_name = 'events'
 
-  # Validations
+  # validations
+
   validates :iam_user_id, :iam_tenant_id, presence: true
   validates :eventable_type, :eventable_id, presence: true
 
-  # Associations
-  belongs_to :eventable, polymorphic: true
+  # associations
 
-  # Callbacks
-  after_create do |event|
-    # Outboxer::Message.backlog(messageable_type: event.class.name, messageable_id: event.id)
-  end
+  belongs_to :eventable, polymorphic: true
 end
