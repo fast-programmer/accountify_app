937a3d2c6b77c5796eae7e060af64dbba22b709e
bedrock-adam <adammikulas@gmail.com>
2024-12-14 22:06:40 +1100

    update spec

--- app/jobs/outboxer_integration/message/publish_job.rb
+++ app/jobs/outboxer_integration/message/publish_job.rb
@@ -6,47 +6,51 @@ module OutboxerIntegration
       sidekiq_options queue: 'events', retry: false, backtrace: true
 
       def perform(args)
+        messageable = ActiveRecord::Base.connection_pool.with_connection do
+          args['messageable_type'].constantize.find(args['messageable_id'])
+        end
+
         case args['messageable_type']
         when 'Accountify::Organisation::CreatedEvent'
           Accountify::InvoiceStatusSummary::GenerateJob.perform_async({
-            'tenant_id' => args['tenant_id'],
-            'organisation_id' => args['organisation_id'] })
+            'tenant_id' => messageable.tenant_id,
+            'organisation_id' => messageable.body['organisation']['id'] })
 
         when 'Accountify::Invoice::DraftedEvent'
           Accountify::InvoiceStatusSummary::RegenerateJob.perform_async({
-            'tenant_id' => args['tenant_id'],
-            'organisation_id' => args['organisation_id'],
-            'invoice_updated_at' => args['occurred_at'] })
+            'tenant_id' => messageable.tenant_id,
+            'organisation_id' => messageable.body['organisation']['id'],
+            'invoice_updated_at' => messageable.created_at.utc.iso8601 })
 
         when 'Accountify::Invoice::UpdatedEvent'
           Accountify::InvoiceStatusSummary::RegenerateJob.perform_async({
-            'tenant_id' => args['tenant_id'],
-            'organisation_id' => args['organisation_id'],
-            'invoice_updated_at' => args['occurred_at'] })
+            'tenant_id' => messageable.tenant_id,
+            'organisation_id' => messageable.body['organisation']['id'],
+            'invoice_updated_at' => messageable.created_at.utc.iso8601 })
 
         when 'Accountify::Invoice::IssuedEvent'
           Accountify::InvoiceStatusSummary::RegenerateJob.perform_async({
-            'tenant_id' => args['tenant_id'],
-            'organisation_id' => args['organisation_id'],
-            'invoice_updated_at' => args['occurred_at'] })
+            'tenant_id' => messageable.tenant_id,
+            'organisation_id' => messageable.body['organisation']['id'],
+            'invoice_updated_at' => messageable.created_at.utc.iso8601 })
 
         when 'Accountify::Invoice::PaidEvent'
           Accountify::InvoiceStatusSummary::RegenerateJob.perform_async({
-            'tenant_id' => args['tenant_id'],
-            'organisation_id' => args['organisation_id'],
-            'invoice_updated_at' => args['occurred_at'] })
+            'tenant_id' => messageable.tenant_id,
+            'organisation_id' => messageable.body['organisation']['id'],
+            'invoice_updated_at' => messageable.created_at.utc.iso8601 })
 
         when 'Accountify::Invoice::VoidedEvent'
           Accountify::InvoiceStatusSummary::RegenerateJob.perform_async({
-            'tenant_id' => args['tenant_id'],
-            'organisation_id' => args['organisation_id'],
-            'invoice_updated_at' => args['occurred_at'] })
+            'tenant_id' => messageable.tenant_id,
+            'organisation_id' => messageable.body['organisation']['id'],
+            'invoice_updated_at' => messageable.created_at.utc.iso8601 })
 
         when 'Accountify::Invoice::DeletedEvent'
           Accountify::InvoiceStatusSummary::RegenerateJob.perform_async({
-            'tenant_id' => args['tenant_id'],
-            'organisation_id' => args['organisation_id'],
-            'invoice_updated_at' => args['occurred_at'] })
+            'tenant_id' => messageable.tenant_id,
+            'organisation_id' => messageable.body['organisation']['id'],
+            'invoice_updated_at' => messageable.created_at.utc.iso8601 })
         end
       end
     end
--- spec/jobs/outboxer_integration/message/publish_job_spec.rb
+++ spec/jobs/outboxer_integration/message/publish_job_spec.rb
@@ -3,13 +3,39 @@ require 'rails_helper'
 module OutboxerIntegration
   module Message
     RSpec.describe PublishJob, type: :job do
-      let(:tenant_id) { 555 }
+      let(:user_id) { 123 }
+      let(:tenant_id) { 456 }
+
+      let(:current_time) { Time.now }
+
+      let(:accountify_organisation) do
+        create(:accountify_organisation, tenant_id: tenant_id)
+      end
+
+      let(:accountify_contact) do
+        create(:accountify_contact,
+          tenant_id: tenant_id, organisation_id: organisation.id)
+      end
+
+      let(:accountify_invoice) do
+        create(:accountify_invoice,
+          tenant_id: tenant_id, organisation_id: organisation.id, contact_id: contact.id)
+      end
 
       describe 'when Accountify::Organisation::CreatedEvent' do
+        let(:event) do
+          create(
+            :accountify_organisation_created_event,
+            user_id: user_id,
+            tenant_id: tenant_id,
+            eventable: accountify_organisation,
+            body: { 'organisation' =>  { 'id' => accountify_organisation.id } })
+        end
+
         before do
           PublishJob.new.perform({
-            'tenant_id' => tenant_id,
-            'messageable_type' => 'Accountify::Organisation::CreatedEvent' })
+            'messageable_type' => 'Accountify::Organisation::CreatedEvent',
+            'messageable_id' => event.id })
         end
 
         it 'performs Accountify::InvoiceStatusSummary::GenerateJob async' do
@@ -17,15 +43,27 @@ module OutboxerIntegration
             hash_including(
               'args' => [
                 hash_including(
-                  'tenant_id' => tenant_id )])])
+                  'tenant_id' => tenant_id,
+                  'organisation_id' => accountify_organisation.id )])])
         end
       end
 
       describe 'when Accountify::Invoice::DraftedEvent' do
+        let(:event) do
+          create(
+            :accountify_invoice_drafted_event,
+            user_id: user_id,
+            tenant_id: tenant_id,
+            eventable: accountify_organisation,
+            created_at: current_time.utc,
+            body: {
+              'organisation' =>  { 'id' => accountify_organisation.id } })
+        end
+
         before do
           PublishJob.new.perform({
-            'tenant_id' => tenant_id,
-            'messageable_type' => 'Accountify::Invoice::DraftedEvent' })
+            'messageable_type' => 'Accountify::Invoice::DraftedEvent',
+            'messageable_id' => event.id })
         end
 
         it 'performs Accountify::InvoiceStatusSummary::RegenerateJob async' do
@@ -33,15 +71,28 @@ module OutboxerIntegration
             hash_including(
               'args' => [
                 hash_including(
-                  'tenant_id' => tenant_id )])])
+                  'tenant_id' => tenant_id,
+                  'organisation_id' => accountify_organisation.id,
+                  'invoice_updated_at' => event.created_at.utc.iso8601 )])])
         end
       end
 
       describe 'when Accountify::Invoice::UpdatedEvent' do
+        let(:event) do
+          create(
+            :accountify_invoice_updated_event,
+            user_id: user_id,
+            tenant_id: tenant_id,
+            eventable: accountify_organisation,
+            created_at: current_time.utc,
+            body: {
+              'organisation' =>  { 'id' => accountify_organisation.id } })
+        end
+
         before do
           PublishJob.new.perform({
-            'tenant_id' => tenant_id,
-            'messageable_type' => 'Accountify::Invoice::UpdatedEvent' })
+            'messageable_type' => 'Accountify::Invoice::UpdatedEvent',
+            'messageable_id' => event.id })
         end
 
         it 'performs Accountify::InvoiceStatusSummary::RegenerateJob async' do
@@ -49,15 +100,28 @@ module OutboxerIntegration
             hash_including(
               'args' => [
                 hash_including(
-                  'tenant_id' => tenant_id )])])
+                  'tenant_id' => tenant_id,
+                  'organisation_id' => accountify_organisation.id,
+                  'invoice_updated_at' => event.created_at.utc.iso8601 )])])
         end
       end
 
       describe 'when Accountify::Invoice::IssuedEvent' do
+        let(:event) do
+          create(
+            :accountify_invoice_issued_event,
+            user_id: user_id,
+            tenant_id: tenant_id,
+            eventable: accountify_organisation,
+            created_at: current_time.utc,
+            body: {
+              'organisation' =>  { 'id' => accountify_organisation.id } })
+        end
+
         before do
           PublishJob.new.perform({
-            'tenant_id' => tenant_id,
-            'messageable_type' => 'Accountify::Invoice::IssuedEvent' })
+            'messageable_type' => 'Accountify::Invoice::IssuedEvent',
+            'messageable_id' => event.id })
         end
 
         it 'performs Accountify::InvoiceStatusSummary::RegenerateJob async' do
@@ -65,15 +129,28 @@ module OutboxerIntegration
             hash_including(
               'args' => [
                 hash_including(
-                  'tenant_id' => tenant_id )])])
+                  'tenant_id' => tenant_id,
+                  'organisation_id' => accountify_organisation.id,
+                  'invoice_updated_at' => event.created_at.utc.iso8601 )])])
         end
       end
 
       describe 'when Accountify::Invoice::PaidEvent' do
+        let(:event) do
+          create(
+            :accountify_invoice_paid_event,
+            user_id: user_id,
+            tenant_id: tenant_id,
+            eventable: accountify_organisation,
+            created_at: current_time.utc,
+            body: {
+              'organisation' =>  { 'id' => accountify_organisation.id } })
+        end
+
         before do
           PublishJob.new.perform({
-            'tenant_id' => tenant_id,
-            'messageable_type' => 'Accountify::Invoice::PaidEvent' })
+            'messageable_type' => 'Accountify::Invoice::PaidEvent',
+            'messageable_id' => event.id })
         end
 
         it 'performs Accountify::InvoiceStatusSummary::RegenerateJob async' do
@@ -81,14 +158,28 @@ module OutboxerIntegration
             hash_including(
               'args' => [
                 hash_including(
-                  'tenant_id' => tenant_id )])])
+                  'tenant_id' => tenant_id,
+                  'organisation_id' => accountify_organisation.id,
+                  'invoice_updated_at' => event.created_at.utc.iso8601 )])])
         end
       end
+
       describe 'when Accountify::Invoice::VoidedEvent' do
+        let(:event) do
+          create(
+            :accountify_invoice_voided_event,
+            user_id: user_id,
+            tenant_id: tenant_id,
+            eventable: accountify_organisation,
+            created_at: current_time.utc,
+            body: {
+              'organisation' =>  { 'id' => accountify_organisation.id } })
+        end
+
         before do
           PublishJob.new.perform({
-            'tenant_id' => tenant_id,
-            'messageable_type' => 'Accountify::Invoice::VoidedEvent' })
+            'messageable_type' => 'Accountify::Invoice::VoidedEvent',
+            'messageable_id' => event.id })
         end
 
         it 'performs Accountify::InvoiceStatusSummary::RegenerateJob async' do
@@ -96,15 +187,28 @@ module OutboxerIntegration
             hash_including(
               'args' => [
                 hash_including(
-                  'tenant_id' => tenant_id )])])
+                  'tenant_id' => tenant_id,
+                  'organisation_id' => accountify_organisation.id,
+                  'invoice_updated_at' => event.created_at.utc.iso8601 )])])
         end
       end
 
       describe 'when Accountify::Invoice::DeletedEvent' do
+        let(:event) do
+          create(
+            :accountify_invoice_deleted_event,
+            user_id: user_id,
+            tenant_id: tenant_id,
+            eventable: accountify_organisation,
+            created_at: current_time.utc,
+            body: {
+              'organisation' =>  { 'id' => accountify_organisation.id } })
+        end
+
         before do
           PublishJob.new.perform({
-            'tenant_id' => tenant_id,
-            'messageable_type' => 'Accountify::Invoice::DeletedEvent' })
+            'messageable_type' => 'Accountify::Invoice::DeletedEvent',
+            'messageable_id' => event.id })
         end
 
         it 'performs Accountify::InvoiceStatusSummary::RegenerateJob async' do
@@ -112,7 +216,9 @@ module OutboxerIntegration
             hash_including(
               'args' => [
                 hash_including(
-                  'tenant_id' => tenant_id )])])
+                  'tenant_id' => tenant_id,
+                  'organisation_id' => accountify_organisation.id,
+                  'invoice_updated_at' => event.created_at.utc.iso8601 )])])
         end
       end
     end
