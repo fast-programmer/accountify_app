ee390c3e4a78ed5702ef1c5a2a341cc14db5f73d
bedrock-adam <adammikulas@gmail.com>
2024-12-21 18:32:32 +1100

    remove provider specific support

--- db/migrate/20240114120424_create_events.rb
+++ db/migrate/20240114120424_create_events.rb
@@ -5,27 +5,12 @@ class CreateEvents < ActiveRecord::Migration[7.0]
       t.bigint :tenant_id, null: false
 
       t.string :type, null: false, limit: 255
-      t.send(json_column_type, :body)
+      t.jsonb :body
       t.datetime :created_at, null: false
 
       t.string :eventable_type, null: false, limit: 255
       t.bigint :eventable_id, null: false
       t.index [:eventable_type, :eventable_id]
-    end
-  end
-
-  private
-
-  def json_column_type
-    case ActiveRecord::Base.connection.adapter_name
-    when /PostgreSQL/
-      :jsonb
-    when /MySQL/, /MariaDB/
-      :json
-    when /SQLite/
-      :text
-    else
-      :json
     end
   end
 end
