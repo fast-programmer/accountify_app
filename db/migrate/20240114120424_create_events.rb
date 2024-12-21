2af92eeeae2339b55512080866aa19d9e0a2b1a8
Adam Mikulasev <adammikulas@gmail.com>
2024-12-21 19:26:33 +1100

    update event schema and model (#49)

--- db/migrate/20240114120424_create_events.rb
+++ db/migrate/20240114120424_create_events.rb
@@ -4,16 +4,13 @@ class CreateEvents < ActiveRecord::Migration[7.0]
       t.bigint :user_id, null: false
       t.bigint :tenant_id, null: false
 
-      t.text :type, null: false
-
-      t.text :eventable_type, null: false
-      t.bigint :eventable_id, null: false
-
+      t.string :type, null: false, limit: 255
       t.jsonb :body
-
       t.datetime :created_at, null: false
-    end
 
-    add_index :events, [:eventable_type, :eventable_id]
+      t.string :eventable_type, null: false, limit: 255
+      t.bigint :eventable_id, null: false
+      t.index [:eventable_type, :eventable_id]
+    end
   end
 end
